2c898342c5842745115aef6bc8d5562e4a0a2352
Jetson_Bigbot <koenvanwijlick@mechatronicmachinery.com>
2025-04-01 13:01:53 +0000

    Update Dockerfile and run_dev script to include new dependencies and environment variable

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -283,6 +283,7 @@ docker run -it --rm \
     -v $ISAAC_ROS_DEV_DIR:/workspaces/isaac_ros-dev \
     -v /etc/localtime:/etc/localtime:ro \
     --name "$CONTAINER_NAME" \
+    --env IP_COMPUTER=192.168.2.11,192.168.2.11 \
     --runtime nvidia \
     --user="admin" \
     --entrypoint /usr/local/bin/scripts/workspace-entrypoint.sh \
